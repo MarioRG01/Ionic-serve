7382ea53ad01a096f44b2ea46396016f962840d5
unknown <CIAC@CIAC1-019.academic.mixcoac.upmx.mx>
2024-05-07 09:56:18 -0600

    sexoal

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -2,7 +2,7 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+        <ion-title>Mario Rodriguez</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
